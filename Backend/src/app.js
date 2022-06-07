522988714b0e0fe7abf0fc8adb464b7efe81f7e8
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-06-07 12:05:37 -0300

    DONE: inicio documentação

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const api = require('./Routes/api');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', index);
 app.use('/api', api);
 
