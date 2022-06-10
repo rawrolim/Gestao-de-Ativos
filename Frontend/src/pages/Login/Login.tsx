bf8957a57b93b689daaf8cd37caf0d44b016f9a1
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-06-10 09:54:33 -0300

    Definir como a primeira pagina a de login

--- Frontend/src/pages/Login/Login.tsx
+++ Frontend/src/pages/Login/Login.tsx
@@ -1,8 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import './Login.css';
 
-const Home: React.FC = () => {
+const Login: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -16,10 +15,9 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Home;
+export default Login;
